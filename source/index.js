22d3da7bb8379c43e5e8c7c081464cd0eb4086f6
Alexander Beletsky <alexander.beletsky@gmail.com>
2021-07-16 12:54:10 +0200

    eslint issues

--- source/index.js
+++ source/index.js
@@ -1,9 +1,11 @@
-import Joi from 'joi';
+import Joi from "joi";
 
-const RouteSettingsSchema = Joi.object().keys({
-  shouldApplyHandler: Joi.func().arity(1),
-  handler: Joi.func().arity(2)
-}).optional();
+const RouteSettingsSchema = Joi.object()
+  .keys({
+    shouldApplyHandler: Joi.func().arity(1),
+    handler: Joi.func().arity(2),
+  })
+  .optional();
 
 /**
  * iterate over all server routes and call `iterator` with each route.
@@ -11,12 +13,12 @@ const RouteSettingsSchema = Joi.object().keys({
  * @param {Hapi.Server} server - hapi server
  * @param {function(route: Hapi.Route)} iterator - iterator function called with each route
  */
-function forEachRoute(server, iterator) {
+const forEachRoute = (server, iterator) => {
   const connections = server.table();
-  connections.forEach(function (connection) {
+  connections.forEach((connection) => {
     connection.table.forEach(iterator);
   });
-}
+};
 
 /**
  * create a new handler function, that checks the given `shouldApplyHandler` function and
@@ -28,39 +30,45 @@ function forEachRoute(server, iterator) {
  * @param {function(request: Hapi.Request, reply: Hapi.Reply)} origHandler - handler called when test is false
  * @return {function(request: Hapi.Request, reply: Hapi.Reply)} wrapped handler
  */
-function wrapHandler({ shouldApplyHandler, handler }, origHandler) {
-  return function (request, reply) {
+const wrapHandler =
+  ({ shouldApplyHandler, handler }, origHandler) =>
+  (request, reply) => {
     if (shouldApplyHandler(request)) {
       return handler.call(this, request, reply);
     }
 
     return origHandler.call(this, request, reply);
   };
-}
 
 const plugin = {
   register(server, options, next) {
     // wrap configured handlers when the server is started
-    server.ext('onPreStart', function (server, next) {
+    server.ext("onPreStart", (server, next) => {
       forEachRoute(server, (route) => {
         const { plugins } = route.settings;
-        if (!plugins || !plugins['hapi-demo']) {
+        if (!plugins || !plugins["hapi-demo"]) {
           return;
         }
 
-        const routeSettings = Joi.attempt(plugins['hapi-demo'], RouteSettingsSchema);
-        route.settings.handler = wrapHandler(routeSettings, route.settings.handler);
+        const routeSettings = Joi.attempt(
+          plugins["hapi-demo"],
+          RouteSettingsSchema
+        );
+        route.settings.handler = wrapHandler(
+          routeSettings,
+          route.settings.handler
+        );
       });
 
       return next();
     });
 
     return next();
-  }
+  },
 };
 
 plugin.register.attributes = {
-  name: 'hapi-demo'
+  name: "hapi-demo",
 };
 
 export default plugin;
--- test/integration/hapi-demo.js
+++ test/integration/hapi-demo.js
@@ -1,151 +1,152 @@
-import assert from 'assert';
-import Hapi from 'hapi';
-import Joi from 'joi';
+/* eslint func-style: 0 */
 
-import HapiDemo from '../../source';
+import assert from "assert";
+import Hapi from "hapi";
+import Joi from "joi";
 
-describe('[integration] hapi-demo', function () {
+import HapiDemo from "../../source";
+
+describe("[integration] hapi-demo", () => {
   let server;
 
-  before('create server', function () {
+  before("create server", () => {
     server = new Hapi.Server();
 
     return server.connection({
-      host: 'localhost',
-      port: 8080
+      host: "localhost",
+      port: 8080,
     });
   });
 
-  before('register hapi-demo plugin', function () {
-    return server.register(HapiDemo);
-  });
+  before("register hapi-demo plugin", () => server.register(HapiDemo));
 
   function demoTest(request) {
     return request.query.demo;
   }
 
   function testHandler(request, reply) {
-    return reply('test route called');
+    return reply("test route called");
   }
 
   function test2Handler(request, reply) {
-    return reply('test2 route called');
+    return reply("test2 route called");
   }
 
-  before('add routes', function () {
-    return server.route([
+  before("add routes", () =>
+    server.route([
       {
-        method: 'GET',
-        path: '/test',
+        method: "GET",
+        path: "/test",
         handler: testHandler,
         config: {
           plugins: {
-            'hapi-demo': {
+            "hapi-demo": {
               shouldApplyHandler: demoTest,
-              handler(request, reply) { return reply('demo route called'); }
-            }
+              handler(request, reply) {
+                return reply("demo route called");
+              },
+            },
           },
           validate: {
             query: Joi.object().keys({
-              demo: Joi.boolean()
-            })
-          }
-        }
+              demo: Joi.boolean(),
+            }),
+          },
+        },
       },
       {
-        method: 'GET',
-        path: '/test2',
-        handler: test2Handler
-      }
-    ]);
-  });
+        method: "GET",
+        path: "/test2",
+        handler: test2Handler,
+      },
+    ])
+  );
 
-  before('initialize server', function () {
-    return server.initialize();
-  });
+  before("initialize server", () => server.initialize());
 
-  after('stop the server', function () {
-    return server.stop();
-  });
+  after("stop the server", () => server.stop());
 
-  describe('for routes that configure the hapi-demo plugin', function () {
+  describe("for routes that configure the hapi-demo plugin", () => {
     let configuredHandler;
 
-    before('find route setup in server configuration', function () {
+    before("find route setup in server configuration", () => {
       const { table } = server.table()[0];
-      const route = table.filter((route) => route.path === '/test')[0];
+      const route = table.filter((route) => route.path === "/test")[0];
 
       configuredHandler = route.settings.handler;
     });
 
-    it('should wrap the handler function', function () {
+    it("should wrap the handler function", () => {
       assert.notEqual(configuredHandler, testHandler);
     });
   });
 
-  describe('when demo condition does not match', function () {
+  describe("when demo condition does not match", () => {
     let response;
 
-    before('call server', function () {
-      return server.inject({
-        method: 'GET',
-        url: '/test'
-      })
-      .then((res) => {
-        response = res;
-      });
-    });
+    before("call server", () =>
+      server
+        .inject({
+          method: "GET",
+          url: "/test",
+        })
+        .then((res) => {
+          response = res;
+        })
+    );
 
-    it('should call the normal handler', function () {
-      assert.equal(response.payload, 'test route called');
+    it("should call the normal handler", () => {
+      assert.equal(response.payload, "test route called");
     });
   });
 
-  describe('when demo condition matches', function () {
+  describe("when demo condition matches", () => {
     let response;
 
-    before('call server', function () {
-      return server.inject({
-        method: 'GET',
-        url: '/test?demo=true'
-      })
-      .then((res) => {
-        response = res;
-      });
-    });
+    before("call server", () =>
+      server
+        .inject({
+          method: "GET",
+          url: "/test?demo=true",
+        })
+        .then((res) => {
+          response = res;
+        })
+    );
 
-    it('should call the demo handler', function () {
-      assert.equal(response.payload, 'demo route called');
+    it("should call the demo handler", () => {
+      assert.equal(response.payload, "demo route called");
     });
   });
 
-  describe('for not-configured routes', function () {
+  describe("for not-configured routes", () => {
     let response;
     let configuredHandler;
 
-    before('call server', function () {
-      return server.inject({
-        method: 'GET',
-        url: '/test2'
-      })
-      .then((res) => {
-        response = res;
-      });
-    });
+    before("call server", () =>
+      server
+        .inject({
+          method: "GET",
+          url: "/test2",
+        })
+        .then((res) => {
+          response = res;
+        })
+    );
 
-    before('find route setup in server configuration', function () {
+    before("find route setup in server configuration", () => {
       const { table } = server.table()[0];
-      const route = table.filter((route) => route.path === '/test2')[0];
+      const route = table.filter((route) => route.path === "/test2")[0];
 
       configuredHandler = route.settings.handler;
     });
 
-    it('should not wrap the handler function', function () {
+    it("should not wrap the handler function", () => {
       assert.equal(configuredHandler, test2Handler);
     });
 
-    it('should call the normal handler', function () {
-      assert.equal(response.payload, 'test2 route called');
+    it("should call the normal handler", () => {
+      assert.equal(response.payload, "test2 route called");
     });
   });
 });
